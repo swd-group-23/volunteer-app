29119508d8fcd821194c864cfeb814a50019cca6
04AJ <alanjcd2004@gmail.com>
2024-09-05 23:17:26 -0500

    corrected deployment error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,22 +3,11 @@ import NavBar from './components/NavBar';
 import './App.css'
 
 function App() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const menuItems = [
-    "Login",
-    "Profile",
-    "Admin",
-    "Volunteer"
-  ];
 
   return (
-
         <>
-          
           <NavBar/>
           <h1 className='text-3xl'>Volunteer App</h1>
-          
-
         </>
   )
 }
