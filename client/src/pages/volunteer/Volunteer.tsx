4c3234ebd4c060e18ff861708a464d8fb340ec17
Alina Bangash <alinabangash2004@gmail.com>
2024-09-06 18:32:48 -0500

    sample table

--- client/src/pages/volunteer/Volunteer.tsx
+++ client/src/pages/volunteer/Volunteer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import NavBar from '../../components/NavBar';
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from "@nextui-org/react";
 import '../../index.css'
 
 const rootElement = document.getElementById('root');
@@ -12,6 +13,35 @@ if (rootElement) {
             <main className='dark text-foreground bg-background'>
                 <NavBar/>
                 <h1 className='text-3xl'>Volunteer Page</h1>
+                <Table aria-label="Example static collection table">
+      <TableHeader>
+        <TableColumn>NAME</TableColumn>
+        <TableColumn>ROLE</TableColumn>
+        <TableColumn>STATUS</TableColumn>
+      </TableHeader>
+      <TableBody>
+        <TableRow key="1">
+          <TableCell>Tony Reichert</TableCell>
+          <TableCell>CEO</TableCell>
+          <TableCell>Active</TableCell>
+        </TableRow>
+        <TableRow key="2">
+          <TableCell>Zoey Lang</TableCell>
+          <TableCell>Technical Lead</TableCell>
+          <TableCell>Paused</TableCell>
+        </TableRow>
+        <TableRow key="3">
+          <TableCell>Jane Fisher</TableCell>
+          <TableCell>Senior Developer</TableCell>
+          <TableCell>Active</TableCell>
+        </TableRow>
+        <TableRow key="4">
+          <TableCell>William Howard</TableCell>
+          <TableCell>Community Manager</TableCell>
+          <TableCell>Vacation</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
             </main>        
         </React.StrictMode>
     );
