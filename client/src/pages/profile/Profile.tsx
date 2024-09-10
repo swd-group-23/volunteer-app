d5c674710bf78721d231bca9431bdf0040a91208
Josh Mathew <joshmathew2022@gmail.com>
2024-09-09 19:26:41 -0500

    user profile management form

--- client/src/pages/profile/Profile.tsx
+++ client/src/pages/profile/Profile.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import NavBar from '../../components/NavBar';
 import '../../index.css';
 import 'react-datepicker/dist/react-datepicker.css';
 import ProfileManagementForm from './ProfileManagementForm';
 const rootElement = document.getElementById('root');
+
 
 if (rootElement) {
     const root = createRoot(rootElement);
