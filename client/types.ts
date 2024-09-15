f35655dff9b7e05af340eb9ab300cd4abf9b9fc9
04AJ <alanjcd2004@gmail.com>
2024-09-15 00:16:08 -0500

    completed matching form and notif component

--- client/types.ts
+++ client/types.ts
@@ -1,5 +1,27 @@
-export interface UserInfo {
+export interface User {
+    id: string;
     name: string;
     email: string;
-    date: Date;
+    password: string;
+    address1: string;
+    address2: string;
+    city: string;
+    state: string;
+    zip: number;
+    skills: string[];
+    preferences?: string;
+    availability: Date[];
+};
+
+export interface Event {
+    id: string;
+    name: string;
+    description: string;
+    address: string;
+    city: string;
+    state: string;
+    zip: number;
+    dateTime: Date;
+    skills: string[];
+    urgency: string;
 };
