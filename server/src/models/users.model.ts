07d16eaea35ec990849898050e53d58a70323da6
04AJ <alanjcd2004@gmail.com>
2024-09-24 11:54:07 -0500

    integrated typescript with express

---
+++ server/src/models/users.model.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id: number;
+    email: string;
+    username: string;
+}
+
+export interface CreateUserRequest {
+    username: string;
+    email: string;
+    password: string;
+}
