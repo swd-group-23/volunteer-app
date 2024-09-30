c57555b70dca5761173a8a1734f9049a2406cf49
04AJ <alanjcd2004@gmail.com>
2024-09-30 18:22:30 -0500

    testing env

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -7,7 +7,6 @@ const router = Router();
 router.get('/', getUsers);
 router.get('/login', loginUser);
 router.get('/:id', getUsersById);
-router.get('/login', loginUser);
 router.post('/', createUser);
 
 
