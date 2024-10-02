54305fbdbe8a3b8e77f1217dd4bf9a39de20bd63
04AJ <alanjcd2004@gmail.com>
2024-10-02 10:42:57 -0500

    testing

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,11 @@ const app = express();
 app.use((_req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  
+  if (_req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
 
   next();
 });
