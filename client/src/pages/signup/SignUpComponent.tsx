06bd82cd7cebf7f618c6d0a3714a62d9362f737b
04AJ <alanjcd2004@gmail.com>
2024-10-07 19:19:03 -0500

    update

--- client/src/pages/signup/SignUpComponent.tsx
+++ client/src/pages/signup/SignUpComponent.tsx
@@ -5,10 +5,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import {EyeFilledIcon} from "../../assets/EyeFilledIcon";
 import {EyeSlashFilledIcon} from "../../assets/EyeSlashFilledIcon";
 import React, { useEffect, useState } from "react";
-import React, { useEffect, useState } from "react";
 import { useUser } from '../../hooks/useUser';
-import axios from 'axios';
-import { User } from '../../../types';
 import axios from 'axios';
 import { User } from '../../../types';
 
@@ -38,25 +35,6 @@ const SignUpComponent = () => {
   } = useForm<Schema>({
     resolver: zodResolver(schema),
   });
-  const env = import.meta.env.VITE_REACT_APP_NODE_ENV;
-  const base_url = (env == 'production') ?  import.meta.env.VITE_REACT_APP_SERVER_BASE_URL : (env == 'staging') ? import.meta.env.VITE_REACT_APP_SERVER_BASE_URL_STAGE : import.meta.env.VITE_REACT_APP_SERVER_BASE_URL_DEV;
-  const [users, setUsers] = useState<User[]>();
-
-
-  useEffect(() => {
-    axios.get<User[]>(`${base_url}/api/users`)
-        .then(response => {
-
-            if (response.data) {
-                setUsers(response.data);
-            }
-
-        })
-        .catch(error => {
-          console.log(error);
-        })
-}
-    , [users]);
   const env = import.meta.env.VITE_REACT_APP_NODE_ENV;
   const base_url = (env == 'production') ?  import.meta.env.VITE_REACT_APP_SERVER_BASE_URL : (env == 'staging') ? import.meta.env.VITE_REACT_APP_SERVER_BASE_URL_STAGE : import.meta.env.VITE_REACT_APP_SERVER_BASE_URL_DEV;
   const [users, setUsers] = useState<User[]>();
@@ -151,79 +129,11 @@ const SignUpComponent = () => {
                   errorMessage={errors.password?.message}
                   isInvalid={errors.password? true : false}
                   {...field}
-    <div className="flex flex-col gap-2 items-center justify-center">
-      <div>
-        <h2 className='text-xl'>Sign Up</h2>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col items-start gap-4"
-        >
-          <div className="flex flex-col mt-4 w-full max-w-xs gap-4">
-            <Controller
-              name="email"
-              control={control}
-              render={({ field }) => (
-                <Input
-                  label="Email"
-                  placeholder="Enter your email"
-                  variant="bordered"
-                  description="We'll never share your email with anyone else."
-                  errorMessage={errors.email?.message}
-                  isInvalid={errors.email ? true : false}
-                  {...field}
-                />
-              )}
-            />
-            
-            <Controller
-              name="password"
-              control={control}
-              render={({ field }) => (
-                  <Input
-                  label="Password"
-                  variant="bordered"
-                  placeholder="Enter your password"
-                  endContent={
-                    <button className="focus:outline-none" type="button" onClick={toggleVisibility} aria-label="toggle password visibility">
-                      {isVisible ? (
-                        <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                      ) : (
-                        <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                      )}
-                    </button>
-                  }
-                  type={isVisible ? "text" : "password"}
-                  className="max-w-xs"
-                  errorMessage={errors.password?.message}
-                  isInvalid={errors.password? true : false}
-                  {...field}
 
                 />
               )}
             />
-                />
-              )}
-            />
 
-              <Controller
-              name="confirmPassword"
-              control={control}
-              render={({ field }) => (
-                  <Input
-                  label="Confirm Password"
-                  variant="bordered"
-                  placeholder="Re-enter your password"
-                  endContent={
-                    <button className="focus:outline-none" type="button" onClick={toggleVisibility} aria-label="toggle password visibility">
-                      {isVisible ? (
-                        <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                      ) : (
-                        <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                      )}
-                    </button>
-                  }
-                  type={isVisible ? "text" : "password"}
-                  className="max-w-xs"
               <Controller
               name="confirmPassword"
               control={control}
@@ -248,46 +158,15 @@ const SignUpComponent = () => {
                   errorMessage={errors.confirmPassword?.message}
                   isInvalid={errors.confirmPassword? true : false}
                   {...field}
-                  onClear={() => setValue('password', '')}
-                  errorMessage={errors.confirmPassword?.message}
-                  isInvalid={errors.confirmPassword? true : false}
-                  {...field}
 
-                />
-              )}
-            />
                 />
               )}
             />
 
           </div>
-          </div>
 
           
-          
 
-          <Button type="submit" color="primary">
-            Sign Up
-          </Button>
-        </form>
-      </div>
-      <div>
-        <h1 className='text-xl'>User List (From backend):</h1>
-        <div>
-          {
-            (users) ? 
-            <ul className="list-disc">
-              {
-              users.map((user) => <li key={user.id}>{user.email}</li>)
-              }
-          
-            </ul>
-           :
-            <></>
-          }
-        </div>
-      </div>
-    
           <Button type="submit" color="primary">
             Sign Up
           </Button>
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -14,4 +14,4 @@ router.delete('/:id', deleteUser);
 router.patch('/:id', updateUser)
 
 
-export default router;
+export default router;  
