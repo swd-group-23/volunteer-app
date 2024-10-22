413413c70ddc7a1676dee2a1e57b9244c33973dc
Alan Johnson <85972796+04AJ@users.noreply.github.com>
2024-10-22 02:30:19 -0500

    Features/alan (#42)
    * first commit
    
    * FIRST COMMIT
    
    * yeehaw
    
    * first commit
    
    * first commit
    
    * corrected nextui config
    
    * corrected deployment error
    
    * test
    
    * sample table
    
    * updated admin
    
    * added sample form
    
    * corrected deployment error
    
    * user profile management form
    
    * hookform-resolver
    
    * bug
    
    * updated files
    
    * Made Login Form
    
    * completed matching form and notif component
    
    * Profile Management Form
    
    * bug
    
    * Event Management Form
    
    * multi-select dropdown
    
    * added multi-select calendar
    
    * tablee
    
    * delete extra files
    
    * remove gitignore
    
    * data
    
    * created sign up form, resolved confirm password
    
    * updated color themes
    
    * added state management
    
    * testing deployment
    
    * dummy data moved
    
    * corrected styling
    
    * fixed deployment
    
    * corrected vite build
    
    * integrated typescript with express
    
    * removed index.js
    
    * allow cors
    
    * more
    
    * test secrets
    
    * prune docker system
    
    * testing cors
    
    * testing deployed base url
    
    * test base url
    
    * test
    
    * handlers/model/route
    
    * new version
    
    * get/post/delete events
    
    * testing env
    
    * create volunteer
    
    * resolved post and get for notifications
    
    * test
    
    * update dockerfile
    
    * correct url
    
    * update master workflow
    
    * update
    
    * corrected nextui config
    
    * updated files
    
    * added state management
    
    * user profile management form
    
    * Profile Management Form
    
    * Event Management Form
    
    * multi-select dropdown
    
    * added multi-select calendar
    
    * delete extra files
    
    * remove gitignore
    
    * data
    
    * testing deployment
    
    * corrected styling
    
    * sample table
    
    * test base url
    
    * testing env
    
    * fix deployment
    
    * fixing cors bug
    
    * testing cors again
    
    * testing
    
    * update docker build
    
    * updated handlers and add to data on post
    
    * remove User dummy data
    
    * delete handler
    
    * hi
    
    * connection, hopefully i did this right lol
    
    * added unit tests
    
    * serverside validation
    
    * connected Event Managment form to backend
    
    * connected volunteer profile form to backend
    
    * misc
    
    * deploy error
    
    * reolved navbar notifications to connect to backend
    
    * update
    
    * updates
    
    * updates
    
    * created notifications.test handler and notifications mock for unit testing
    
    * fixed build issue
    
    * resolved notificationsbyid
    
    * volunteer/event unit testing
    
    * volunteer validation
    
    * event validation
    
    * volunteer unit testing
    
    * change
    
    * updates
    
    * updated frontend
    
    * update unit testing
    
    * this
    
    * resolved deleteNotification, delete Notification unit testing, delete notifs connected to data.ts
    
    * profile management form pulls data from backend
    
    * resolved axios error on frontend, notifications connected to data.ts
    
    * newer unit testing thingy
    
    * added volunteer profile management to admin side
    
    * improved code coverage
    
    * fixed event management form types
    
    * implemented delete user cascade
    
    * impleted delelete event, notif, post notif, and get users
    
    * corrected histories coverage
    
    * updated readme
    
    * testing backend deployment
    
    * added mongodb connection and corrected useEffects
    
    * corrected workflow command
    
    ---------
    
    Co-authored-by: Josh Mathew <joshmathew2022@gmail.com>
    Co-authored-by: Pickled-Mangos <121989847+Pickled-Mangos@users.noreply.github.com>
    Co-authored-by: Alina Bangash <alinabangash@Alinas-MacBook-Pro-3.local>
    Co-authored-by: Alina Bangash <alinabangash2004@gmail.com>

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { getUsers, getUsersById, createUser, loginUser, deleteUser} from "../handlers/users";
+import { getUsers, getUsersById, createUser, loginUser, deleteUser, getUsersMongo} from "../handlers/users";
 import {checkSchema} from 'express-validator';
 import { createUserValidationSchema } from "../utils/validationSchemas";
 
@@ -8,9 +8,11 @@ const router = Router();
 
 router.get('/', getUsers);
 router.post('/login', loginUser);
+router.get('/mongo', getUsersMongo);
 router.get('/:id', getUsersById);
 router.post('/', checkSchema(createUserValidationSchema), createUser);
 router.delete('/:id', deleteUser);
+
 
 
 export default router;  
