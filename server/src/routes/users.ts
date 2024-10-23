c0ec5215ab9adebb0c5ce06db9ee2df696728d78
04AJ <alanjcd2004@gmail.com>
2024-10-22 21:58:10 -0500

    added mongodb handlers

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,14 +1,22 @@
 import {Router} from "express";
-import { getUsers, getUsersById, createUser, loginUser, deleteUser, getUsersMongo} from "../handlers/users";
+import { getUsers, getUsersById, createUser, loginUser, deleteUser, getUsersMongo, getUsersByIdMongo, loginUserMongo, createUserMongo, updateUserMongo, deleteUserMongo} from "../handlers/users";
 import {checkSchema} from 'express-validator';
 import { createUserValidationSchema } from "../utils/validationSchemas";
 
 
 const router = Router();
 
+// mongo routes
+router.get('/mongo', getUsersMongo);
+router.get('/mongo/:id', getUsersByIdMongo);
+router.post('/login/mongo', loginUserMongo);
+router.post('/mongo', checkSchema(createUserValidationSchema), createUserMongo);
+router.patch('/mongo', updateUserMongo)
+router.delete('/mongo/:id', deleteUserMongo);
+
+
 router.get('/', getUsers);
 router.post('/login', loginUser);
-router.get('/mongo', getUsersMongo);
 router.get('/:id', getUsersById);
 router.post('/', checkSchema(createUserValidationSchema), createUser);
 router.delete('/:id', deleteUser);
