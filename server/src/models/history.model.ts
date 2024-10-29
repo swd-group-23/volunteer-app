3466581fa6ff196b0072fc93cc1af753817dd480
Alina Bangash <140352800+alinabangash@users.noreply.github.com>
2024-10-28 19:52:59 -0500

    Features/alina (#46)
    * first commit
    
    * first commit
    
    * sample table
    
    * tablee
    
    * dummy data moved
    
    * more
    
    * handlers/model/route
    
    * new version
    
    * hi
    
    * connection, hopefully i did this right lol
    
    * updates
    
    * volunteer unit testing
    
    * change
    
    * update unit testing
    
    * this
    
    * newer unit testing thingy
    
    * new routes, handlers, and other -> connected to mongo
    
    * updated history handlers
    
    ---------
    
    Co-authored-by: Alina Bangash <alinabangash@Alinas-MacBook-Pro-3.local>
    Co-authored-by: 04AJ <alanjcd2004@gmail.com>

--- server/src/models/history.model.ts
+++ server/src/models/history.model.ts
@@ -1,3 +1,5 @@
+
+import { ObjectId } from "mongodb";
 export interface History {
     id: string;
     volunteerId: string;
@@ -18,3 +20,36 @@ export interface GetHistoryResponse {
     eventDate: Date;
     status: string[];
   }
+
+  export interface MongoHistory {
+    volunteerId: ObjectId;
+    eventId: ObjectId;
+    status: string[];
+    _id?: ObjectId;
+}
+
+export interface MongoVolunteer {
+  _id?: ObjectId;
+  userId: ObjectId;
+  name: string;
+  email: string;
+  password: string;
+  address1: string;
+  address2: string;
+  city: string;
+  state: string;
+  zip: number;
+  skills: string[];
+  preferences?: string;
+  availability: Date[];
+};
+
+export interface MongoEvent {
+  _id: ObjectId;
+  name: string;
+  description: string;
+  location: string;
+  dateTime: Date;
+  skills: string[];
+  urgency: string;
+};
