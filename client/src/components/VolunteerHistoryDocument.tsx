db937e3aec1b405ca66cb4dc2ccda17717ee217e
04AJ <alanjcd2004@gmail.com>
2024-11-14 02:26:43 -0600

    added volunteer history report

---
+++ client/src/components/VolunteerHistoryDocument.tsx
@@ -0,0 +1,195 @@
+import { Page, Text, View, Document } from "@react-pdf/renderer";
+import {Style} from "@react-pdf/types"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Volunteer } from "../../types";
+
+const VolunteerHistoryDocument = () => {
+  const [volunteers, setVolunteers] = useState<Volunteer[]>();
+
+  const env = import.meta.env.VITE_REACT_APP_NODE_ENV;
+  const base_url = (env == 'production') ?  import.meta.env.VITE_REACT_APP_SERVER_BASE_URL 
+  : (env == 'staging') ? import.meta.env.VITE_REACT_APP_SERVER_BASE_URL_STAGE 
+  : import.meta.env.VITE_REACT_APP_SERVER_BASE_URL_DEV;
+
+  useEffect(() => {
+    axios.get<Volunteer[]>(`${base_url}/api/volunteers/mongo`)
+        .then(response => {
+            if (response.data) {
+                setVolunteers(response.data);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+      
+  }, []);
+
+  const createTableHeader = () => {
+    return (
+      <View style={tableRowStyle} fixed>
+
+        <View style={firstTableColHeaderStyle}>
+          <Text style={tableCellHeaderStyle}>Volunteer Name</Text>
+        </View>
+
+        <View style={tableColHeaderStyle}>
+          <Text style={tableCellHeaderStyle}>VolunteerID</Text>
+        </View>
+
+        <View style={tableColHeaderStyle}>
+          <Text style={tableCellHeaderStyle}>UserID</Text>
+        </View>
+
+        <View style={tableColHeaderStyle}>
+          <Text style={tableCellHeaderStyle}>Preferences</Text>
+        </View>
+
+        <View style={tableColHeaderStyle}>
+          <Text style={tableCellHeaderStyle}>Skills</Text>
+        </View>
+
+        <View style={tableColHeaderStyle}>
+          <Text style={tableCellHeaderStyle}>Participated Events</Text>
+        </View>
+
+        <View style={tableColHeaderStyle}>
+          <Text style={tableCellHeaderStyle}>No Show Events</Text>
+        </View>
+
+        <View style={tableColHeaderStyle}>
+          <Text style={tableCellHeaderStyle}>Scheduled Events</Text>
+        </View>
+      </View>
+    );
+  };
+
+
+  return (
+    <Document>
+      <Page
+        style={pageStyle}
+        size="A4"
+        orientation="portrait">
+
+        <View style={tableStyle}>
+          {createTableHeader()}
+          {
+            volunteers?.map(volunteer => 
+            (  <View style={tableRowStyle}>
+                  <View style={firstTableColStyle}>
+                    <Text style={tableCellStyle}>{volunteer.name}</Text>
+                  </View>
+
+                  <View style={tableColStyle}>
+                    <Text style={tableCellIDStyle}>{volunteer._id}</Text>
+                  </View>
+
+                  <View style={tableColStyle}>
+                    <Text style={tableCellIDStyle}>{volunteer.userId}</Text>
+                  </View>
+
+                  <View style={tableColStyle}>
+                    <Text style={tableCellStyle}>{volunteer.preferences?.toString()}</Text>
+                  </View>
+
+                  <View style={tableColStyle}>
+                    <Text style={tableCellStyle}>{volunteer.skills.toString()}</Text>
+                  </View>
+
+                  <View style={tableColStyle}>
+                    <Text style={tableCellStyle}>Element</Text>
+                  </View>
+
+                  <View style={tableColStyle}>
+                    <Text style={tableCellStyle}>Element</Text>
+                  </View>
+
+                  <View style={tableColStyle}>
+                    <Text style={tableCellStyle}>Element</Text>
+                  </View>
+                </View>)
+
+            )
+          }
+         
+
+        </View>
+
+      </Page>
+    </Document>
+  );
+
+};
+
+const pageStyle: Style = {
+  paddingTop: 16,
+  paddingHorizontal: 40,
+  paddingBottom: 56
+};
+
+const tableStyle: Style = {
+  display: "flex",
+  width: "auto"
+};
+
+const tableRowStyle: Style = {
+  flexDirection: "row"
+};
+
+const firstTableColHeaderStyle: Style = {
+  width: "20%",
+  borderStyle: "solid",
+  borderColor: "#000",
+  borderBottomColor: "#000",
+  borderWidth: 1,
+  backgroundColor: "#bdbdbd"
+};
+
+const tableColHeaderStyle: Style = {
+  width: "20%",
+  borderStyle: "solid",
+  borderColor: "#000",
+  borderBottomColor: "#000",
+  borderWidth: 1,
+  borderLeftWidth: 0,
+  backgroundColor: "#bdbdbd"
+};
+
+const firstTableColStyle: Style = {
+  width: "20%",
+  borderStyle: "solid",
+  borderColor: "#000",
+  borderWidth: 1,
+  borderTopWidth: 0
+};
+
+const tableColStyle: Style = {
+  width: "20%",
+  borderStyle: "solid",
+  borderColor: "#000",
+  borderWidth: 1,
+  borderLeftWidth: 0,
+  borderTopWidth: 0
+};
+
+const tableCellHeaderStyle: Style = {
+  textAlign: "center",
+  margin: 2,
+  fontSize: 10,
+  fontWeight: "bold"
+};
+
+const tableCellStyle: Style = {
+  textAlign: "center",
+  margin: 3,
+  fontSize: 9
+};
+
+const tableCellIDStyle: Style = {
+  textAlign: "left",
+  margin: 0,
+  fontSize: 6
+};
+
+export default VolunteerHistoryDocument;
